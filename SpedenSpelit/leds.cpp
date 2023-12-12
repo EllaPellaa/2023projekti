9bf50921cc766ba650d2a38afe1cdcc06b95ab41
Kurppa <89580030+ArttuIivari@users.noreply.github.com>
2023-12-12 13:43:51 +0200

    valmis

--- SpedenSpelit/leds.cpp
+++ SpedenSpelit/leds.cpp
@@ -1,15 +1,57 @@
-#ifndef LEDS_H
-#define LEDS_H
+#include "leds.h"
 #include "Arduino.h"
 
-//Kerrotaan, minkä nimisiä aliohjelmia sekä muuttujia tullaan käyttämään.
+//*********************************
+//Alustetaan analogiset pinnit A2-A5
+//*********************************
 
-void initializeLeds();
+void initializeLeds(){
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(A4, OUTPUT);
+  pinMode(A5, OUTPUT);
+}
+//***************************************************************
+//Haetaan pääohjelmasta numeron perusteella, mikä ledeistä sytytetään
+//***************************************************************
+void setLed(byte ledNumber){
+  switch(ledNumber){
+    case 0: 
+      digitalWrite(A2,HIGH);
+      break;
+    case 1: 
+      digitalWrite(A3,HIGH);
+      break;    
+    case 2: 
+      digitalWrite(A4,HIGH);
+      break;
+    case 3: 
+      digitalWrite(A5,HIGH);
+      break;
+    default:
+      break;
+  }
+  
+//******************
+//sammuta kaikki ledit
+//******************
+}
+void clearAllLeds(void){
 
-void setLed(byte ledNumber);
+  digitalWrite(A2,LOW);
+  digitalWrite(A3,LOW);
+  digitalWrite(A4,LOW);
+  digitalWrite(A5,LOW);
+}
 
-void clearAllLeds(void);
+//******************
+//Sytytä kaikki valot
+//*****************
 
-void setAllLeds(void);
-
-#endif
+void setAllLeds(void){
+  
+  digitalWrite(A2,HIGH);
+  digitalWrite(A3,HIGH);
+  digitalWrite(A4,HIGH);
+  digitalWrite(A5,HIGH);
+}
