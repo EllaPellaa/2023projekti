32103b188ac571856db81e9e31239e3811bb0769
Eketus <erik.kristian.leskinen@gmail.com>
2023-12-12 13:22:22 +0200

    PinMode napeille

--- SpedenSpelit/buttons.cpp
+++ SpedenSpelit/buttons.cpp
@@ -1,9 +1,16 @@
 #include "buttons.h"
 
 
+
 //höhöö
 
 void initButtonsAndButtonInterrupts(void)
 {
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+
+  pinMode(6, INPUT_PULLUP);
   // See requirements for this function from buttons.h
 }
